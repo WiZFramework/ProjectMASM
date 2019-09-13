9c330c4f13999808aa16e238c19c96c2598028de
YamnoiYasushi <wiz.yamanoi@wiz.ac.jp>
2019-09-13 14:03:26 +0900

    Sample101追加

---
+++ Sample101/Sample101/Source.asm
@@ -0,0 +1,22 @@
+
+includelib libcmt.lib
+includelib libvcruntime.lib
+includelib libucrt.lib
+includelib legacy_stdio_definitions.lib
+
+.386
+.model flat, c
+.stack 100h
+printf  PROTO arg1:Ptr Byte
+
+.data
+msg1    byte "Hello World!", 0Ah, 0
+
+.code
+main    proc
+INVOKE printf, ADDR msg1
+mov	eax, 0
+ret
+
+main    endp
+end
